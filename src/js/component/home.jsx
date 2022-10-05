1741085311bdd9d1928211ba602907c189772efd
lauramagallanes <pasocenturiontours@gmail.com>
2022-10-05 20:54:19 +0000

    primer commit de to do list con react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	const [tarea, setTarea] = useState("");
+	const [listaTareas, setListaTareas] = useState([]);
+
+	function insertarTareas(e) {
+		// setListaTareas(listaTareas => listaTareas.concat(tarea));
+		if (e.key === 'Enter') {
+			e.preventDefault();
+
+			setListaTareas([...listaTareas, tarea]); //copia lo anterior y le agrega lo nuevo
+			console.log(listaTareas);
+			setTarea("");
+
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="container d-flex justify-content-center text-center">
+				<input className="form-control w-50" type="text" placeholder="Tareas" aria-label="Disabled input example" value={tarea} onChange={(e) => setTarea(e.target.value)} onKeyDown={insertarTareas} />
+			</div>
+			<div className="container d-flex justify-content-center text-left">
+				<ul className="list-group w-50">
+					{listaTareas.map((item, i) => <li id ={i} class="list-group-item" key={i}>{item} </li>)};
+				</ul>
+
+			</div>
 		</div>
 	);
 };
