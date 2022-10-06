74f0a43f9f2ba1cd5888599d9961636d2c71d221
lauramagallanes <pasocenturiontours@gmail.com>
2022-10-06 17:24:04 +0000

    agregue alerta de lista vacia

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,15 +9,24 @@ const Home = () => {
 	const [tarea, setTarea] = useState("");
 	const [listaTareas, setListaTareas] = useState([]);
 
+	
+
 	//comienza funcion de crear tareas
 	function insertarTareas(e) {
 		// setListaTareas(listaTareas => listaTareas.concat(tarea));
 		if (e.key === 'Enter') {
 			e.preventDefault();
-
+			if (tarea == "") {
+				
+				
+				alert(`No has ingresado una tarea`);
+								
+			  }else{
 			setListaTareas([...listaTareas, tarea]); //copia lo anterior y le agrega lo nuevo
 			console.log(listaTareas);
 			setTarea("");
+		}
+			
 
 		}
 	}
@@ -36,13 +45,14 @@ function eliminarTareas(id) {
 	return (
 		<div>
 			<h1>TODOS</h1>
+			
 			<div className="container d-flex justify-content-center text-center">
-				<input className="form-control w-50" type="text" placeholder="Tareas" aria-label="Disabled input example" value={tarea} onChange={(e) => setTarea(e.target.value)} onKeyDown={insertarTareas} />
+				<input className="form-control w-50" type="text" placeholder="Tareas" aria-label="Disabled input example" value={tarea} onChange={(e) => setTarea(e.target.value)} onKeyDown={insertarTareas} required/>
 			</div>
 			<div className="container d-flex justify-content-center text-left">
 				<ul className="list-group w-50">
 					
-					{listaTareas.map((item, id) => <li class="list-group-item" key={id}>{item}<button type="button" className="btn btn-info float-end nuevoBoton" onClick={() => eliminarTareas(id)}>X</button> </li>)}
+					{listaTareas.map((item, id) => <li className="list-group-item" key={id}>{item}<button type="button" className="btn btn-info float-end nuevoBoton" onClick={() => eliminarTareas(id)}>X</button> </li>)}
 					
 				</ul>
 
