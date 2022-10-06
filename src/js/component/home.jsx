7dc4733a247670f5f4e31be53aceb8a03c37fb92
lauramagallanes <pasocenturiontours@gmail.com>
2022-10-06 15:29:56 +0000

    agregado de la logica para eliminar elementos de la lista y css para el boton

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import '../../styles/index.css';
 
 
 
@@ -8,6 +9,7 @@ const Home = () => {
 	const [tarea, setTarea] = useState("");
 	const [listaTareas, setListaTareas] = useState([]);
 
+	//comienza funcion de crear tareas
 	function insertarTareas(e) {
 		// setListaTareas(listaTareas => listaTareas.concat(tarea));
 		if (e.key === 'Enter') {
@@ -19,15 +21,29 @@ const Home = () => {
 
 		}
 	}
+//finaliza funcion de crear tareas
 
+//comienza funcion de eliminar tareas
+function eliminarTareas(id) {
+	
+	//el id corresponde al elemento sobre el que hago click
+	console.log(id);                                  // aca le estoy indicando que me incluya en la lista todos los elementos, menos el que tiene la id del elemento sobre el que hago click
+	setListaTareas(listaTareas.filter((item, index) => index !== id)) 
+	console.log(listaTareas);
+	
+}
+//finaliza funcion de eliminar tareas
 	return (
 		<div>
+			<h1>TODOS</h1>
 			<div className="container d-flex justify-content-center text-center">
 				<input className="form-control w-50" type="text" placeholder="Tareas" aria-label="Disabled input example" value={tarea} onChange={(e) => setTarea(e.target.value)} onKeyDown={insertarTareas} />
 			</div>
 			<div className="container d-flex justify-content-center text-left">
 				<ul className="list-group w-50">
-					{listaTareas.map((item, i) => <li id ={i} class="list-group-item" key={i}>{item} </li>)};
+					
+					{listaTareas.map((item, id) => <li class="list-group-item" key={id}>{item}<button type="button" className="btn btn-info float-end nuevoBoton" onClick={() => eliminarTareas(id)}>X</button> </li>)}
+					
 				</ul>
 
 			</div>
